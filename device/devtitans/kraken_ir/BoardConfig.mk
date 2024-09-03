84b6f76b82dc96e3b6a6218f0fcc6cbf500441a0
erlonbie <erlonminievix@gmail.com>
2024-09-02 21:19:44 -0400

    first commit

---
+++ device/devtitans/kraken_ir/BoardConfig.mk
@@ -0,0 +1,2 @@
+# Inclui as configurações do "hardware" do emulador
+include build/make/target/board/emulator_x86_64/BoardConfig.mk
---
+++ device/devtitans/kraken_ir/devtitans_kraken_ir.mk
@@ -0,0 +1,8 @@
+# Herda as configurações do emulador (produto sdk_phone_x86_64)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_phone_x86_64.mk)
+
+# Sobrescreve algumas variáveis com os dados do novo produto
+PRODUCT_NAME := devtitans_kraken_ir
+PRODUCT_DEVICE := kraken_ir
+PRODUCT_BRAND := KrakenIrBrand
+PRODUCT_MODEL := KrakenIrModel
